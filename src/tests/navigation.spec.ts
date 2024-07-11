4a18ce42edf3552ccbf87182d55ee5fb5d4b5a7d
Ramesh Murugan <rameshmurugan@Rameshs-MacBook-Air.local>
2024-07-11 07:33:43 +0530

    tweaked

--- src/tests/navigation.spec.ts
+++ src/tests/navigation.spec.ts
@@ -1,21 +1,20 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test.describe(`E2E Tests`, () => {
-
+test.describe(`Homepage and Navigation`, () => {
   test("Verify Homepage Loads Correctly", async ({ page }) => {
-    await page.goto("https://magento.softwaretestingboard.com/")
+    await page.goto("https://magento.softwaretestingboard.com/");
     await page.waitForLoadState();
-    await expect(page).toHaveTitle(/Home Page/)
-  })
+    await expect(page).toHaveTitle(/Home Page/);
+  });
 
   test("Verify Logo Link", async ({ page }) => {
-    await page.goto("https://magento.softwaretestingboard.com/")
+    await page.goto("https://magento.softwaretestingboard.com/");
     await page.locator(".logo").click();
-    await expect(page).toHaveURL("https://magento.softwaretestingboard.com")
-  })
+    await expect(page).toHaveURL("https://magento.softwaretestingboard.com");
+  });
 
   test("Verify Navigation Menu Links", async ({ page }) => {
-    await page.goto("https://magento.softwaretestingboard.com/")
+    await page.goto("https://magento.softwaretestingboard.com/");
     await expect(page).toHaveTitle("Home Page");
     await page.locator(".nav-1").click();
     await expect(page).toHaveTitle(/What's New/);
@@ -31,44 +30,45 @@ test.describe(`E2E Tests`, () => {
     await expect(page).toHaveTitle(/Sale/);
     await page.locator(".logo").click();
     await expect(page).toHaveTitle("Home Page");
-  })
+  });
 
   test("Verify Search Functionality", async ({ page }) => {
-
-    const product: string = 'jackets'
-    await page.goto("https://magento.softwaretestingboard.com/")
+    const product: string = "jackets";
+    await page.goto("https://magento.softwaretestingboard.com/");
     await page.locator("#search").fill(product);
     await page.keyboard.press("Enter");
-    await expect(page).toHaveTitle(`Search results for: '${product}'`)
+    await expect(page).toHaveTitle(`Search results for: '${product}'`);
   });
 
   test("Verify Footer Links", async ({ browser }) => {
     const context = await browser.newContext();
-    const page = await context.newPage()
-
+    const page = await context.newPage();
 
     await page.goto("https://magento.softwaretestingboard.com/");
-    await expect(page.locator(".page-footer")).toBeVisible()
-    await page.getByRole("link", { name: "Notes" }).click()
-    const pagePromise = context.waitForEvent('page');
+    await expect(page.locator(".page-footer")).toBeVisible();
+    await page.getByRole("link", { name: "Notes" }).click();
+    const pagePromise = context.waitForEvent("page");
     const newPage = await pagePromise;
-    await expect(newPage).toHaveURL("https://softwaretestingboard.com/magento-store-notes/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=notes_promotion");
+    await expect(newPage).toHaveURL(
+      "https://softwaretestingboard.com/magento-store-notes/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=notes_promotion",
+    );
     await newPage.close();
-    await page.close()
+    await page.close();
   });
 
   test("Verify Newsletter Subscription", async ({ browser }) => {
-
     const context = await browser.newContext();
     const page = await context.newPage();
 
     await page.goto("https://magento.softwaretestingboard.com/");
-    const subscribeLink = page.getByRole('link', { name: "Subscribe" })
+    const subscribeLink = page.getByRole("link", { name: "Subscribe" });
     await expect(subscribeLink).toBeVisible();
     await subscribeLink.click();
-    const pagePromise = context.waitForEvent('page');
+    const pagePromise = context.waitForEvent("page");
     const newPage = await pagePromise;
-    await expect(newPage).toHaveURL("https://softwaretestingboard.com/subscribe/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=email_subscribe");
+    await expect(newPage).toHaveURL(
+      "https://softwaretestingboard.com/subscribe/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=email_subscribe",
+    );
     await newPage.locator("#mce-EMAIL").fill("rameshmtester@gmaikl.com");
     await newPage.locator("#mce-FNAME").fill("Ramesh");
     await newPage.locator("#mce-LNAME").fill("Murugan");
@@ -76,14 +76,14 @@ test.describe(`E2E Tests`, () => {
     await newPage.locator("#mce-POSITION").fill("Skipper");
     await newPage.click("#mc-embedded-subscribe");
     const newPage2 = await pagePromise;
-    await expect(newPage2).toHaveURL("https://softwaretestingboard.com/subscribe/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=email_subscribe")
+    await expect(newPage2).toHaveURL(
+      "https://softwaretestingboard.com/subscribe/?utm_source=magento_store&utm_medium=banner&utm_campaign=notes_promo&utm_id=email_subscribe",
+    );
 
     await newPage.close();
-    await page.close()
+    await page.close();
     //await expect(newPage2.getByText(/We need to confirm your email address. To complete the subscription process, please click the link in the email we just sent you./)).toBeVisible();
     //await expect(newPage2.locator("#mce-success-response")).toBeVisible()
     //await expect(newPage2.locator("#mce-success-response")).toContainText("Almost finished... We need to confirm your email address. To complete the subscription process, please click the link in the email we just sent you.")
   });
-
-
-})
+});
